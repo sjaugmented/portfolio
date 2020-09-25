24e686144c743c67a6f8c0cc393b73a964f83247
sjaugmented <sjaugmented@gmail.com>
2020-09-25 13:02:41 -0700

    + Skills, Projects, About

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
-import logo from "./logo.svg"
+import { Route, Link, Switch } from "react-router-dom"
 import "./App.css"
-import Nav from "./components/Nav"
+import { Nav, Skills, Projects, About } from "./components"
 
 function App() {
   return (
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Nav from "./Nav"
+import Projects from "./Projects"
+import Skills from "./Skills"
+import About from "./About"
+
+export { Nav, Projects, Skills, About }
