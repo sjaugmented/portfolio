35da8fc04118c2b18e73fabeec2c3a8a6db6be7b
sjaugmented <sjaugmented@gmail.com>
2020-09-25 13:30:07 -0700

    basic About

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,55 @@
 import React from "react"
 
 const About = () => {
-  return <div></div>
+  return (
+    <section id="about" className="callout about">
+      <article className="wrap">
+        <h2>About Me</h2>
+        <p>
+          Thanks for stopping by! Since you’ve made it this far, allow me to
+          take a minute to tell you a little about who I am.
+        </p>
+
+        <p>
+          My life began with storytelling. It was what always interested me.
+          This lead me from writing to screenwriting to eventually combining my
+          love of story with my love of photography and Mastering in
+          Cinematography and working as a professional Cinematographer for over
+          a decade.
+        </p>
+
+        <p>
+          Recently I wanted to challenge myself with a new skill set. The world
+          runs on software and I’ve always wanted to understand and become
+          proficient with the languages that make our modern world go round.
+          Covid2020 gave me the perfect opportunity to knuckle down and do just
+          that.
+        </p>
+
+        <p>
+          Creative problem solving and team management are the pillars of my
+          professional experience. I thrive in high stress environments and
+          maintain a calm, relaxed, goal-focused attitude. I bring the same
+          level-headed approach to my code and Full Stack development: What is
+          the feeling we want to evoke in the user; what are the limiting
+          factors of the project; what’s the best way forward? These are the
+          central questions that define everything I do.
+        </p>
+
+        <p>
+          When I’m not programming or shooting features or commercials, you can
+          find me hiking, running, or relaxing with the occasional video game or
+          movie.
+        </p>
+
+        <p>
+          If you are looking for a creative Full Stack developer with an eye
+          toward user experience and narrative design, we should talk. We
+          probably have a lot in common.
+        </p>
+      </article>
+    </section>
+  )
 }
 
 export default About
