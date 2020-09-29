a4d3f2b65af78285b27ac5e523cf99177a4552f7
sjaugmented <sjaugmented@gmail.com>
2020-09-29 10:03:14 -0700

    trying to add ssl seal

--- src/components/Foot.js
+++ src/components/Foot.js
@@ -1,10 +1,34 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 const Foot = () => {
+  useEffect(() => {
+    const script = document.createElement("script")
+
+    script.src =
+      "https://seal.godaddy.com/getSeal?sealID=Uwek1tGFGFSG17CiFjCKXC8AOSzQldmfbjWhrKKjIcxT0LaCIotgHcIm3fhq"
+    script.async = true
+    script.type = "text/javascript"
+
+    const siteseal = document.querySelector("#siteseal")
+
+    siteseal.appendChild(script)
+
+    return () => {
+      siteseal.removeChild(script)
+    }
+  }, [])
+
   return (
     <footer>
       <div className="wrap"></div>
       <p className="copyright">Let's move fast and break things.</p>
+      <span id="siteseal">
+        {/* <script
+          async
+          type="text/javascript"
+          src="https://seal.godaddy.com/getSeal?sealID=Uwek1tGFGFSG17CiFjCKXC8AOSzQldmfbjWhrKKjIcxT0LaCIotgHcIm3fhq"
+        ></script> */}
+      </span>
     </footer>
   )
 }
