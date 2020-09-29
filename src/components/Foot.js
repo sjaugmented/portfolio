4c5c4bdca5d993a7394107b91b13e05f9f8de2b3
sjaugmented <sjaugmented@gmail.com>
2020-09-29 11:17:30 -0700

    removed site seal

--- src/components/Foot.js
+++ src/components/Foot.js
@@ -1,28 +1,10 @@
 import React, { useEffect } from "react"
 
 const Foot = () => {
-  useEffect(() => {
-    const script = document.createElement("script")
-
-    script.src =
-      "https://seal.godaddy.com/getSeal?sealID=Uwek1tGFGFSG17CiFjCKXC8AOSzQldmfbjWhrKKjIcxT0LaCIotgHcIm3fhq"
-    script.async = true
-    script.type = "text/javascript"
-
-    const siteseal = document.querySelector("#siteseal")
-
-    siteseal.appendChild(script)
-
-    return () => {
-      siteseal.removeChild(script)
-    }
-  }, [])
-
   return (
     <footer>
       <div className="wrap"></div>
       <p className="copyright">Let's move fast and break things.</p>
-      <span id="siteseal"></span>
     </footer>
   )
 }
