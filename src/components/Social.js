20cac7928ff1aa9a0610bfbfd0e0f06b966db947
sjaugmented <sjaugmented@gmail.com>
2020-09-28 20:16:07 -0700

    socials change color in scroll

--- src/components/Social.js
+++ src/components/Social.js
@@ -2,16 +2,24 @@ import React, { useEffect, useState } from "react"
 import { useScrollPosition } from "../hooks/useScrollPosition"
 
 const Social = () => {
-  const [hideOnScroll, setHideOnScroll] = useState(true)
+  const [screenY, setScreenY] = useState(0)
 
   useScrollPosition(({ prevPos, currPos }) => {
-    console.log(currPos.x)
-    console.log(currPos.y)
+    console.log(screenY)
+
+    setScreenY(currPos.y)
   })
+
+  let style = {}
+
+  screenY < -800
+    ? (style = { color: "rgb(16, 105, 101)" })
+    : (style = { color: "rgb(32, 209, 200)" })
 
   return (
     <div class="social-links">
       <a
+        style={style}
         target="_blank"
         rel="noopener noreferrer"
         href="https://github.com/sjaugmented"
@@ -19,6 +27,7 @@ const Social = () => {
         <i className="fa fa-github" aria-hidden="true"></i>
       </a>
       <a
+        style={style}
         target="_blank"
         rel="noopener noreferrer"
         href="https://www.linkedin.com/in/cineseth"
@@ -26,11 +35,12 @@ const Social = () => {
         <i className="fa fa-linkedin" aria-hidden="true"></i>
       </a>
       <a
+        style={style}
         target="_blank"
         rel="noopener noreferrer"
         href="https://www.instagram.com/sether82/?hl=en"
       >
-        <i class="fa fa-instagram" aria-hidden="true"></i>
+        <i className="fa fa-instagram" aria-hidden="true"></i>
       </a>
     </div>
   )
