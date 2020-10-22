fbdc67ae0e5bfb73122a56ff61b6a492e5be3a85
Seth <sjaugmented@gmail.com>
2020-10-22 14:42:12 -0700

    added printIt field to project data; styling, formatting

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,27 +2,31 @@ import React from "react"
 import "../App.css"
 import ProjectCard from "./ProjectCard"
 import projects from "../data/projects.json"
-// import useWindowSize from "../hooks/useWindowSize"
 import TrackVisibility from "react-on-screen"
 
 const Projects = ({ id }) => {
-  // const size = useWindowSize()
-
   return (
     <section id="gallery">
       <div className="wrap">
         <h2 className="gallery-heading">What I've Done</h2>
         <div>
           {projects.map((project, index) => {
-            let style = {}
-            if (index % 2 === 0) {
-              style = { backgroundColor: "rgb(30, 30, 30)" }
+            if (project.printIt) {
+              let style = {}
+              // if (index % 2 === 0) {
+              //   style = { backgroundColor: "rgb(30, 30, 30)" }
+              // }
+              return (
+                <TrackVisibility
+                  key={index}
+                  once
+                  partialVisibility
+                  offset={-250}
+                >
+                  <ProjectCard style={style} key={index} project={project} />
+                </TrackVisibility>
+              )
             }
-            return (
-              <TrackVisibility key={index} once partialVisibility offset={-250}>
-                <ProjectCard style={style} key={index} project={project} />
-              </TrackVisibility>
-            )
           })}
         </div>
       </div>
